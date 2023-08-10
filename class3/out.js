9f3774c7faea6016d101ca0068b20c9013ad26a6
Erammm <141656121+Erammm@users.noreply.github.com>
2023-08-10 14:16:18 +0400

    Add files via upload

---
+++ class3/out.js
@@ -0,0 +1,166 @@
+// var username="Ahmed";
+// var age =25;
+// var output=`I'm ${username} and my age ${age}`
+// console.log(output)
+
+
+
+
+// function getname(x)
+// {
+//     console.log(x);
+// }
+// getname("Hello")
+// const get_name=x =>console.log(x);
+// get_name("mohamed")
+
+
+
+// const getnumber=number=>
+// {
+//     for(let i=1; i<=number; i++)
+//     {
+//         if(i % 2==0)
+//         {
+//             console.log(`${i} this is even number`);
+//         }
+//         else
+//         {
+//             console.log(`${i} is odd number`);
+
+//         }
+//     }
+// }
+// getnumber(40)
+
+
+
+
+// const number1=[1,2,3,4,5]
+// const number2=[...number1,3,4,5,6,7]
+// number1.push
+// console.log(number2)
+
+
+
+
+
+
+// const user={
+//     username:"NOOH",
+//     age:29,
+//     address:"Boushar"
+// }
+// const{username,...x}=user
+// console.log(x)
+
+
+
+
+// const user=[
+//     {
+//         username:"Waleed",
+//         age:24,
+//         mobile:["90969090","9929954"]
+//     }
+// ]
+// const[{mobile}]=user
+// const[mob1,mob2]=mobile
+// console.log(mob2);
+
+
+
+
+// const number=[1,2,3,4,5,6,7,8,9]
+// number.forEach(function(el,idx){
+// console.log(idx);
+// })
+
+
+
+
+// const name=["Eram","Nooh","Bata","waheed"]
+// name.map(el => console.log(el))
+
+
+
+
+// const user=[
+//     {
+//         username:"Waleed",
+//         age:24,
+//         mobile:["90969090","9929954"]
+//     }]
+// user.map(el => console.log(el.username))
+
+
+
+
+//MAX $$ MIN
+// const number=(num1,num2,num3) => {
+//     if(num1>num2 && num2>num3)
+//     {
+//        console.log(`${num1} is max number and ${num3} is min number`);
+//     }
+//     else if(num2>num1 && num1>num3)
+//     {
+//         console.log(`${num2} is max number and ${num3} is min number`);
+//     }
+//     else if(num3>num2 && num2>num1 )
+//     {
+//         console.log(`${num3} is max number and ${num1} is min number`);  
+//     }
+//     else if(num3>num2 && num1>num2 )
+//     {
+//         console.log(`${num3} is max number and ${num2} is min number`);  
+//     }
+//     else if(num2>num1 && num3>num2 )
+//     {
+//         console.log(`${num2} is max number and ${num1} is min number`);  
+//     }
+//     else if(num1>num2 && num3>num2 )
+//     {
+//         console.log(`${num1} is max number and ${num2} is min number`);  
+//     }
+//     else
+//     {
+//         console.group("all number are equals")
+//     }
+    
+
+// }
+
+// number(6,5,4)
+
+
+
+
+
+
+//-12
+// const mark=(n1,n2,n3,n4,n5)=>{
+//     total=n1+n2+n3+n4+n5;
+//     averag=total/5
+//     precntage=averag*0.8
+//     console.log(`total is ${total}`)
+//     console.log(`average is ${averag}`)
+//     console.log(`precntage is ${precntage}`)
+// } 
+// mark(90,95,80,88,93)
+
+
+
+
+
+
+// const getmonth=number => {
+//     if(number >0 && number <=12)
+//     {
+//         if(number==1 || number==3 || number==5 || number==7 || number==8 || number==10 || number==12)
+//         {
+//             console.log("this month is 31 days")
+//         }
+//         if()
+//     }
+// }
+
