a8257b76876aae970394c027ffdbea2ee2b4bd2b
Erammm <141656121+Erammm@users.noreply.github.com>
2023-08-15 18:49:40 +0400

    Add files via upload

---
+++ class7/js/script.js
@@ -0,0 +1,23 @@
+let btn=document.getElementById('btn')
+
+btn.addEventListener('click',function(){
+let inp1=document.getElementById("inp1");
+let inp2=document.getElementById("inp2");
+let text=document.getElementById('text');
+let result=parseInt(inp1.value)+parseInt(inp2.value)
+
+text.innerHTML=result
+
+
+})
+let x="5"
+console.log(typeof(+x));
+
+
+
+
+
+
+
+
+
